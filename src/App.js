60299cd459fd660855b386ca24e83cc899e171fe
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-04-30 02:23:15 +0300

    WIP file generation & download

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import html2canvas from 'html2canvas';
+
 class App {
 	onStart() {
 		// TODO: put some code here
+	}
+
+	generateImage(element, format = 'image/png') {
+		return new Promise((resolve) => {
+			html2canvas(element).then(canvas => {
+				resolve(canvas.toDataURL(format));
+			});
+		});
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -8,3 +8,24 @@ import './index.scss';
 
 const app = new App();
 app.onStart();
+
+const image = {format: 'image/jpeg', fileMame: 'wallpaper.jpg'};
+let downloadButton = undefined;
+const button = document.getElementById('btn-download');
+const root = document.getElementById('root');
+button.addEventListener('click', () => {
+	const targetElement = document.querySelector('#wallpaper');
+	app.generateImage(targetElement).then(imageDataUrl => {
+		imageDataUrl = imageDataUrl.replace(image.format, 'image/octet-stream');
+		downloadButton = document.createElement('a');
+		downloadButton.target = '_blank';
+		downloadButton.href = imageDataUrl;
+		downloadButton.download = image.fileMame;
+		var linkText = document.createTextNode('Download 2');
+		downloadButton.appendChild(linkText);
+		// downloadButton.click();
+		root.insertBefore(downloadButton, button);
+	});
+});
+
+
