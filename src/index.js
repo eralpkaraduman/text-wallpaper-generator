1a0e30c673beffacc61db4f0cce78f3f38f42812
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-04-30 18:13:28 +0300

    simplified image downloading

--- src/index.js
+++ src/index.js
@@ -1,39 +1,37 @@
 import 'normalize.css/normalize.css';
-import 'typeface-fira-mono';
 import 'flatui-colors';
 import 'flexboxgrid';
-
-import App from './App';
+import 'typeface-fira-mono';
+import 'blob.js';
+import html2canvas from 'html2canvas';
+import FileSaver from 'file-saver';
 import './index.scss';
 
-const app = new App();
-app.onStart();
+// UTILS
 
-const image = {format: 'image/jpeg', fileMame: 'wallpaper.jpg'};
-let downloadButton = undefined;
-const button = document.getElementById('btn-download');
-const root = document.getElementById('root');
-button.addEventListener('click', () => {
+const callOnNextFrame = callback => () => window.setTimeout(callback, 0);
+
+// IMAGE GENERATION
+let imageOptions = { format: 'image/jpeg', fileName: 'wallpaper.jpg' };
+
+function downloadCanvas(canvas) {
+	canvas.toBlob(blob => FileSaver.saveAs(blob, imageOptions.fileName));
+}
+
+function downloadImage() {
 	const targetElement = document.querySelector('#wallpaper');
-	app.generateImage(targetElement).then(imageDataUrl => {
-		imageDataUrl = imageDataUrl.replace(image.format, 'image/octet-stream');
-		downloadButton = document.createElement('a');
-		downloadButton.target = '_blank';
-		downloadButton.href = imageDataUrl;
-		downloadButton.download = image.fileMame;
-		var linkText = document.createTextNode('Download 2');
-		downloadButton.appendChild(linkText);
-		// downloadButton.click();
-		root.insertBefore(downloadButton, button);
-	});
-});
+	html2canvas(targetElement) // TODO: pass options
+		.then(canvas => downloadCanvas(canvas));
+}
+
+window.onDownloadImage = downloadImage;
 
 // TEXT INPUT
 
 const wallpaperTextInput = document.getElementById('wallpaper-text-input');
 
 wallpaperTextInput.value =
-`TEXT WALLPAPER GENERATOR!✨
+	`TEXT WALLPAPER GENERATOR!✨
 =========================== 
 
 Perfect for writing notes and reminders,
@@ -46,7 +44,6 @@ Select text color and size,
 Select background color and size,
 Click DOWNLOAD button above!`;
 
-const callOnNextFrame = callback => () => window.setTimeout(callback, 0);
 function handleOnTextChanged() {
 	wallpaperTextInput.style.height = 'auto';
 	wallpaperTextInput.style.height = wallpaperTextInput.scrollHeight + 'px';
