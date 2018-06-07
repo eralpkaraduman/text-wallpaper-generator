19981d1d084485ca73c3d7cc63d465b13dded116
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-06-07 18:54:44 +0300

    WIP menu context menus

--- src/Menu.js
+++ src/Menu.js
@@ -2,10 +2,12 @@ import * as utils from './utils';
 
 export default class Menu {
 	menuElement = null;
+	menuContainerElement = null;
 	width = null;
 	height = null;
 	scale = null;
 	color = null;
+	menuWindowElement = null;
 	
 	constructor({onDownloadClicked}) {
 		this.handleOnColorButtonClicked = null;
@@ -17,21 +19,27 @@ export default class Menu {
 	}
 	
 	onStart = () => {
+		this.menuContainerElement = utils.getElement('menu-container');
 		this.menuElement = utils.getElement('menu');
+		this.menuWindowElement = utils.getElement('menu-window');
 
 		// initial state
 		this.width =  window.screen.width;
 		this.height =  window.screen.height;
 		this.scale = window.devicePixelRatio;
 		
+		const textSizeButtonElement = utils.getElement('menu-button-text-size-increase');
+		
 		const buttons = {
 			'menu-button-color-button': this.handleOnColorButtonClicked,
-			'menu-button-text-size-increase': this.handleOnIncreaseFontSizeButtonClicked,
+			// 'menu-button-text-size-increase': this.handleOnIncreaseFontSizeButtonClicked,
+			'menu-button-text-size-increase': () => this.onToggleMenuWindow(textSizeButtonElement),
 			'menu-button-text-size-decrease': this.handleOnDecreaseFontSizeButtonClicked,
 			'menu-button-image-size-width': this.handleOnImageWidthButtonClicked,
 			'menu-button-image-size-height': this.handleOnImageHeightButtonClicked,
 			'menu-button-download': this.handleOnDownloadButtonClicked
 		};
+		
 		
 		utils.kvoIndexed(buttons).forEach(({key, value}) => {
 			const element = utils.getElement(key);
@@ -40,11 +48,31 @@ export default class Menu {
 	}
 	
 	onShow = () => {
-		this.menuElement.style.display = 'flex';
+		this.menuContainerElement.style.display = 'flex';
 	}
 	
 	onHide = () => {
-		this.menuElement.style.display = 'none';
+		this.menuContainerElement.style.display = 'none';
+	}
+	
+	onToggleMenuWindow = (buttonElement) => {
+		
+		//TODO: deselect all
+		buttonElement.classList.remove('active-button');
+		
+		const active = this.menuElement.classList.contains('menu-active');
+		if (active) {
+			this.menuElement.classList.remove('menu-active');
+			this.menuElement.classList.add('menu');
+			this.menuWindowElement.style.display = 'none';
+		}
+		else {
+			this.menuElement.classList.remove('menu');
+			this.menuElement.classList.add('menu-active');
+			this.menuWindowElement.style.display = 'flex';
+
+			buttonElement.classList.add('active-button');
+		}
 	}
 }
 
