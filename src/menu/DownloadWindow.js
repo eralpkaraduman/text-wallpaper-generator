b2b0f6b18cf70972189765ab37148d6250c2d454
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-07-20 16:58:55 +0300

    download window activity indicator

--- src/menu/DownloadWindow.js
+++ src/menu/DownloadWindow.js
@@ -11,6 +11,7 @@ export default class DownloadWindow extends MenuWindow {
 
 	_downloadButtonElement: HTMLAnchorElement;
 	_imageElement: HTMLImageElement;
+	_activityIndicatorElement: HTMLElement;
 	_onGenerateImage: GenerateCanvasCallback;
 	_onGenerateFileName: GenerateFileNameCallback;
 
@@ -18,27 +19,36 @@ export default class DownloadWindow extends MenuWindow {
 		super(elementId);
 		this._onGenerateImage = onGenerateImage;
 		this._onGenerateFileName = onGenerateFileName;
-		const buttonElement = utils.getElement('download-window-download-button');
-		this._downloadButtonElement = ((buttonElement: any): HTMLAnchorElement);
+		// const buttonElement = utils.getElement('download-window-download-button');
+		// this._downloadButtonElement = ((buttonElement: any): HTMLAnchorElement);
 		const imageElement = utils.getElement('download-window-image');
 		this._imageElement = ((imageElement: any): HTMLImageElement);
+		this._imageElement.addEventListener('load', this.onImageLoaded);
+		this._activityIndicatorElement = utils.getElement('image-activity-indicator-container');
 	}
 
 	async updateImage() {
 		const canvas = await this._onGenerateImage();
 		const fileName = this._onGenerateFileName();
 		const dataUrl = canvas.toDataURL('image/jpeg');
-		this._downloadButtonElement.download = this._onGenerateFileName();
-		this._downloadButtonElement.href = dataUrl;
+		// this._downloadButtonElement.download = this._onGenerateFileName();
+		// this._downloadButtonElement.href = dataUrl;
 		this._imageElement.src = dataUrl;
 		this._imageElement.alt = fileName;
 	}
-
+	
 	clearImage() {
-		this._downloadButtonElement.download = '';
-		this._downloadButtonElement.href = 'javascript:undefined';
+		// this._downloadButtonElement.download = '';
+		// this._downloadButtonElement.href = 'javascript:undefined';
 		this._imageElement.src = '';
 		this._imageElement.alt = '';
+		this._imageElement.style.opacity = '0.0';
+		this._activityIndicatorElement.style.display = 'block';
+	}
+
+	onImageLoaded = () => {
+		this._imageElement.style.opacity = '1.0';
+		this._activityIndicatorElement.style.display = 'none';
 	}
 	
 	onWindowWillOpen(): void {
