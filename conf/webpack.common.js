630700cffe64999ab931442c2214e82c30689d48
Eralp Karaduman <eralp@eralpkaraduman.com>
2025-09-08 12:34:19 +0300

    fix mobile safari

--- conf/webpack.common.js
+++ conf/webpack.common.js
@@ -66,6 +66,7 @@ module.exports = {
               loader: 'sass-loader',
               options: {
                 sourceMap: true,
+                implementation: require('sass'),
               },
             },
           ],
